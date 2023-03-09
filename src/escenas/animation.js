7b9e224c1154fc6388d7f9bd722a636a9a8e1627
MigueldeAreba <mdeareba@ucm.es>
2023-03-09 15:46:36 +0100

    Inicial, va mal

---
+++ src/escenas/animation.js
@@ -0,0 +1,111 @@
+import Player from '../objetos/player.js';
+import Floor from '../objetos/floor.js';
+import Box from '../objetos/box.js';
+/**
+ * Escena principal de juego.
+ * @extends Phaser.Scene
+ */
+export default class Animation extends Phaser.Scene {
+	
+	constructor() {
+		super({ key: 'animation' });
+	}
+	
+	preload(){
+		this.load.image('castle', 'assets/castle.gif');
+		this.load.spritesheet('player', 'assets/Player/player.png', {frameWidth: 72, frameHeight: 86})
+		this.load.spritesheet('box', 'assets/Box/box.png', {frameWidth: 64, frameHeight: 64})
+	}
+	
+	/**
+	* Creación de los elementos de la escena principal de juego
+	*/
+	create() {
+		//Imagen de fondo
+		this.add.image(0, 0, 'castle').setOrigin(0, 0);
+
+		let boxes = this.physics.add.group();
+
+		var player1Controls = {
+			left: game.input.keyboard.addKey(Phaser.Keyboard.A),
+			right: game.input.keyboard.addKey(Phaser.Keyboard.D),
+			up: game.input.keyboard.addKey(Phaser.Keyboard.W),
+			down: game.input.keyboard.addKey(Phaser.Keyboard.S),
+			fire: game.input.keyboard.addKey(Phaser.Keyboard.SPACE)
+		  };
+
+		  var player2Controls = {
+			left: game.input.keyboard.addKey(Phaser.Keyboard.LEFT),
+			right: game.input.keyboard.addKey(Phaser.Keyboard.RIGHT),
+			up: game.input.keyboard.addKey(Phaser.Keyboard.UP),
+			down: game.input.keyboard.addKey(Phaser.Keyboard.DOWN),
+			fire: game.input.keyboard.addKey(Phaser.Keyboard.ENTER)
+		  };
+		
+		//Instanciamos nuestro personaje, que es un caballero, y la plataforma invisible que hace de suelo
+		let player1 = new Player(this, 50, 0, player1Controls);
+		let player2 = new Player(this, 30, 0, player2Controls);
+		let floor = new Floor(this, 50);
+		let box1 = new Box(this, 200, 0, boxes);	
+		let box2 = new Box(this, 400, 0, boxes);
+
+		player1.body.onCollide = true; // Activamos onCollide para poder detectar la colisión del caballero con el suelo
+		player2.body.onCollide = true; // Activamos onCollide para poder detectar la colisión del caballero con el suelo
+
+
+		let scene = this; // Nos guardamos una referencia a la escena para usarla en la función anidada que viene a continuación
+		
+		this.physics.add.collider(player1, floor, function(){
+			if(scene.physics.world.overlap(player1, floor)) {
+				player1.enableJump(); // Hemos tocado el suelo, permitimos volver a saltar
+			}
+		});
+
+		this.physics.add.collider(player2, floor, function(){
+			if(scene.physics.world.overlap(player2, floor)) {
+				player2.enableJump(); // Hemos tocado el suelo, permitimos volver a saltar
+			}
+		});
+
+		this.physics.add.collider(floor, boxes);
+		this.physics.add.collider(player1, boxes);
+		this.physics.add.collider(player2, boxes);
+
+		/*
+		 * Escuchamos los eventos de colisión en el mundo para poder actuar ante ellos
+		 * En este caso queremos detectar cuando el caballero colisiona con el suelo para activar el salto del personaje
+		 * El salto del caballero lo desactivamos en su "clase" (archivo knight.js) para evitar dobles saltos
+		 * También comprobamos si está en contacto con alguna caja mientras ataca, en ese caso destruimos la caja
+		 */
+		scene.physics.world.on('collide', function(gameObject1, gameObject2, body1, body2) {
+			if(gameObject1 === player1 && gameObject2 === floor || gameObject1 === floor && gameObject2 === player1){
+				player1.enableJump();
+			}
+
+			
+
+			if(gameObject1 === player1 && boxes.contains(gameObject2)){
+				if(gameObject1.isAttackInProcess()) {
+					gameObject2.destroyMe()
+				} 				
+			}
+		});	
+
+		scene.physics.world.on('collide', function(gameObject1, gameObject2, body1, body2) {
+			if(gameObject1 === player2 && gameObject2 === floor || gameObject1 === floor && gameObject2 === player2){
+				player2.enableJump();
+			}
+
+			
+
+			if(gameObject1 === player2 && boxes.contains(gameObject2)){
+				if(gameObject1.isAttackInProcess()) {
+					gameObject2.destroyMe()
+				} 				
+			}
+		});	
+
+		this.scene.launch('title');
+	}
+
+}
--- src/game.js
+++ src/game.js
@@ -1,58 +1,50 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game-container', { preload: preload, create: create, update: update });
+import Animation from './escenas/animation.js';
+import Title from './escenas/title.js'
+/**
+ * Inicio del juego en Phaser. Creamos el archivo de configuración del juego y creamos
+ * la clase Game de Phaser, encargada de crear e iniciar el juego.
+ * Doc: https://photonstorm.github.io/phaser3-docs/Phaser.Types.Core.html#.GameConfig
+ */
+let config = {
+    type: Phaser.AUTO,
+    parent: 'juego',
+    // type: Phaser.CANVAS,
+    // canvas: document.getElementById("juego"),
+    width:  656,
+    height: 376,
+    pixelArt: true,
+	scale: {
+		autoCenter: Phaser.Scale.CENTER_HORIZONTALLY,
+		// Configuramos phaser para que se adapte al tamaño de pantalla donde ejecutadmos
+		// con un mínimo y un máximo de tamaño
+		mode: Phaser.Scale.FIT,
+		min: {
+            width: 328,
+            height: 188
+        },
+		max: {
+            width: 1312,
+            height: 752
+        },
+		zoom: 1
+    },
+    scene: [Title, Animation],
+    physics: { 
+        default: 'arcade', 
+        arcade: { 
+            gravity: { y: 200 }, 
+            debug: true 
+        },
+        checkCollision: {
+            up: true,
+            down: true,
+            left: true,
+            right: true
+        }
+    },
+    title: "Prueba de concepto",
+    version: "1.0.0",
+    transparent: false
+};
 
-var player1;
-var player2;
-var platforms;
-
-function preload() {
-  game.load.image('player1', './assets/jugador1.png');
-  game.load.image('player2', './assets/jugador2.png');
-  game.load.image('platform', './assets/platform.png');
-
-  // cargamos las pistas de audio
-  game.load.audio('jump', './assets/jump.mp3');
-  game.load.audio('shoot', './assets/shoot.wav');
-}
-
-function create() {
-  // creamos las plataformas
-  platforms = game.add.group();
-  platforms.enableBody = true;
-  var platform = platforms.create(0, game.world.height - 64, 'platform');
-  platform.scale.setTo(2, 2);
-  platform.body.immovable = true;
-
-  // creamos a los jugadores
-  var player1Controls = {
-    left: game.input.keyboard.addKey(Phaser.Keyboard.A),
-    right: game.input.keyboard.addKey(Phaser.Keyboard.D),
-    up: game.input.keyboard.addKey(Phaser.Keyboard.W),
-    down: game.input.keyboard.addKey(Phaser.Keyboard.S),
-    fire: game.input.keyboard.addKey(Phaser.Keyboard.F)
-  };
-  player1 = new Player(game, 100, game.world.height - 150, 'player1', player1Controls);
-  player1.weapon = new Weapon(game, 'bullet', 10, player1.sprite);
-
-  var player2Controls = {
-    left: game.input.keyboard.addKey(Phaser.Keyboard.LEFT),
-    right: game.input.keyboard.addKey(Phaser.Keyboard.RIGHT),
-    up: game.input.keyboard.addKey(Phaser.Keyboard.UP),
-    down: game.input.keyboard.addKey(Phaser.Keyboard.DOWN),
-    fire: game.input.keyboard.addKey(Phaser.Keyboard.ENTER)
-  };
-  player2 = new Player(game, 500, game.world.height - 150, 'player2', player2Controls);
-  player2.weapon = new Weapon(game, 'bullet', 10, player2.sprite);
-}
-
-function update() {
-  // comprobamos si los jugadores se están tocando
-  game.physics.arcade.collide(player1.sprite, player2.sprite);
-
-  // comprobamos las colisiones de los jugadores con las plataformas
-  game.physics.arcade.collide(player1.sprite, platforms);
-  game.physics.arcade.collide(player2.sprite, platforms);
-
-  // actualizamos los jugadores
-  player1.update();
-  player2.update();
-}
+new Phaser.Game(config);
