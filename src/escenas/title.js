29d8ef6895aad49a6e186a4c3701d76d764907fd
gavilaneees <davidgd2001@hotmail.es>
2023-03-30 13:16:18 +0200

    cambio de fondo

--- src/escenas/title.js
+++ src/escenas/title.js
@@ -16,7 +16,7 @@ export default class Title extends Phaser.Scene {
 	 */
 	preload(){
 		this.load.image('start', 'assets/GUI/start.png');
-		this.load.image('fondo', 'assets/PixelArt/backgroundPlataformas.png');
+		this.load.image('fondo1', 'assets/PixelArt/backgroundStart.png');
 		this.load.image('logo', 'assets/images/logo1-2.png');
 	}
 	
@@ -25,7 +25,7 @@ export default class Title extends Phaser.Scene {
 	*/
 	create() {
 		//Pintamos un fondo
-		var back = this.add.image(0, 0, 'fondo').setOrigin(0, 0);
+		var back = this.add.image(0, 0, 'fondo1').setOrigin(0, 0);
 
 		//Pintamos un botón de Empezar
 		var sprite = this.add.image(this.sys.game.canvas.width/2, this.sys.game.canvas.height/1.5, 'start')
