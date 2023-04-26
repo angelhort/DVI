7cd5ba624ab6a7529f868179e0bed3be7a8bb50f
gavilaneees <davidgd2001@hotmail.es>
2023-04-26 13:24:42 +0200

    irmandinio en menu

--- src/escenas/characterSelection.js
+++ src/escenas/characterSelection.js
@@ -32,6 +32,7 @@ export default class CharacterSelection extends Phaser.Scene {
         this.load.spritesheet('amancio', 'assets/PixelArt/amancioAnimaciones.png', {frameWidth: 48, frameHeight: 48})
 		this.load.spritesheet('rajoy', 'assets/PixelArt/rajoyAnimaciones.png', {frameWidth: 48, frameHeight: 48})
         this.load.spritesheet('rosalia', 'assets/PixelArt/rosaliaAnimaciones.png', {frameWidth: 48, frameHeight: 48})
+        this.load.spritesheet('irmandinio', 'assets/PixelArt/irmandinioAnimaciones.png', {frameWidth: 48, frameHeight: 48})
         this.loadFont('font', 'assets/webfonts/AncientModernTales.otf');
         this.load.audio('miAudio5', './assets/sonidos/backgroundsound.mp3');
         this.load.audio('miAudio10', './assets/sonidos/click.mp3');
@@ -48,14 +49,16 @@ export default class CharacterSelection extends Phaser.Scene {
         const characterSelectionMenu = this.add.container();
 
         // Crear los sprites de los personajes
-        const amancio = this.add.sprite(this.cameras.main.centerX - 200, this.cameras.main.centerY, 'amancio', 0);
-        const rajoy = this.add.sprite(this.cameras.main.centerX + 0, this.cameras.main.centerY, 'rajoy', 0);
-        const rosalia = this.add.sprite(this.cameras.main.centerX + 200, this.cameras.main.centerY, 'rosalia', 0);
+        const amancio = this.add.sprite(this.cameras.main.centerX - 80, this.cameras.main.centerY, 'amancio', 0);
+        const rajoy = this.add.sprite(this.cameras.main.centerX + 80, this.cameras.main.centerY, 'rajoy', 0);
+        const rosalia = this.add.sprite(this.cameras.main.centerX + 250, this.cameras.main.centerY, 'rosalia', 0);
+        const irmandinio = this.add.sprite(this.cameras.main.centerX - 250, this.cameras.main.centerY, 'irmandinio', 0);
 
         // Añadir texto debajo de cada sprite
         const amancioText = this.add.text(amancio.x+5, amancio.y + amancio.height / 2 + 10, 'Amancio', { fontSize: '16px', color: '#ffffff', fontFamily: 'font' }).setOrigin(0.5, 0);
         const rajoyText = this.add.text(rajoy.x+5, rajoy.y + rajoy.height / 2 + 10, 'Rajoy', { fontSize: '16px', color: '#ffffff', fontFamily: 'font' }).setOrigin(0.5, 0);
         const rosaliaText = this.add.text(rosalia.x+5, rosalia.y + rosalia.height / 2 + 10, 'Rosalia De Castro', { fontSize: '16px', color: '#ffffff', fontFamily: 'font' }).setOrigin(0.5, 0);
+        const irmandinioText = this.add.text(irmandinio.x+5, irmandinio.y + irmandinio.height / 2 + 10, 'Irmandiño', { fontSize: '16px', color: '#ffffff', fontFamily: 'font' }).setOrigin(0.5, 0);
 
         // Texto para la selección de jugador
         this.playerText = this.add.text(this.cameras.main.centerX, 50, 'Selecciona tu personaje Jugador 1', { fontSize: '48px', color: '#ffffff', fontFamily: 'font' }).setOrigin(0.5, 0);
@@ -79,14 +82,22 @@ export default class CharacterSelection extends Phaser.Scene {
             frameRate: 2,
             repeat: -1
         });
+        this.anims.create({
+            key: 'idleirmandinio',
+            frames: this.anims.generateFrameNumbers('irmandinio', { start: 0, end: 1 }),
+            frameRate: 2,
+            repeat: -1
+        });
         amancio.anims.play('idleamancio');
         rajoy.anims.play('idlerajoy');
         rosalia.anims.play('idlerosalia');
+        irmandinio.anims.play('idleirmandinio');
 
         // Añadir interactividad a los sprites
         amancio.setInteractive();
         rajoy.setInteractive();
         rosalia.setInteractive();
+        irmandinio.setInteractive();
 
         // Permitir a los jugadores elegir personajes
         let player1Turn = true;
@@ -211,6 +222,40 @@ export default class CharacterSelection extends Phaser.Scene {
             }
         });
 
+        irmandinio.on('pointerdown', pointer => {
+            this.miAudio10 = this.sound.add('miAudio10');
+			this.miAudio10.play();
+            if (player1Turn) {
+                player1Character = 'irmandinio';
+                player1Bullets = 'hoja';
+                player1Turn = false;
+                player2Turn = true;
+                player1AjusteAlcance = 0.7;
+                player1AjusteCadencia = 0.4;
+                player1AjusteVelocidad = 1;
+                this.playerText.setText('Selecciona tu personaje Jugador 2');
+            } else if (player2Turn) {
+                player2Character = 'irmandinio';
+                player2Bullets = 'hoja';
+                player2AjusteAlcance = 0.7;
+                player2AjusteCadencia = 0.4;
+                player2AjusteVelocidad = 1;
+                this.scene.launch('sceneSelection', {
+                    player1Character,
+                    player2Character,
+                    player1Bullets,
+                    player2Bullets,
+                    player1AjusteAlcance,
+                    player2AjusteAlcance,
+                    player1AjusteVelocidad,
+                    player2AjusteVelocidad,
+                    player1AjusteCadencia,
+                    player2AjusteCadencia,
+                    miAudioAux
+                });
+            }
+        });
+
         // Añadir los sprites al menú
         characterSelectionMenu.add(amancio);
         characterSelectionMenu.add(rajoy);
@@ -218,6 +263,8 @@ export default class CharacterSelection extends Phaser.Scene {
         characterSelectionMenu.add(rajoyText);
         characterSelectionMenu.add(rosalia);
         characterSelectionMenu.add(rosaliaText);
+        characterSelectionMenu.add(irmandinio);
+        characterSelectionMenu.add(irmandinioText);
 
         // Sonido
 
